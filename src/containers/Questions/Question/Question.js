05457798c9b71b17b2244ee9f0a9707c384e4091
Zviad Sichinava <zviad.sichinava@devfactory.com>
2018-04-22 16:55:09 +0200

    feat: implement question toggle

--- src/containers/Questions/Question/Question.js
+++ src/containers/Questions/Question/Question.js
@@ -3,15 +3,36 @@ import { connect } from 'react-redux'
 import './Question.css';
 
 class Question extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      expanded: false
+    }
+
+    this.handleToggle = this.handleToggle.bind(this)
+  }
+
+  // Handle question toggle
+  handleToggle() {
+    this.setState({ expanded: !this.state.expanded})
+  }
+
   render() {
+    const answer = (
+      <div className="questions__item__answer">
+        {this.props.question.get('answer')}
+      </div>
+    )
+
     return (
       <div className="list-group-item questions__item">
-        <div className="questions__item__question">
+        <div className="questions__item__question"
+             onClick={this.handleToggle}>
           {this.props.question.get('question')}
         </div>
-        <div className="questions__item__answer">
-          {this.props.question.get('answer')}
-        </div>
+        {this.state.expanded && answer}
       </div>
     );
   }
