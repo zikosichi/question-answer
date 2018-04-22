7f5d68023041a3de2f665272d1c9c8b209a748a5
Zviad Sichinava <zviad.sichinava@devfactory.com>
2018-04-22 16:01:53 +0200

    feat: handle inputs change in new question

--- src/containers/NewQuestion/NewQuestion.js
+++ src/containers/NewQuestion/NewQuestion.js
@@ -1,35 +1,77 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import './NewQuestion.css';
 
+// Actions
+import { addNewQuestion } from '../../store/actions'
+
 class NewQuestion extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      question: '',
+      answer: '',
+    };
+
+    this.handleNewQuestion = this.handleNewQuestion.bind(this)
+    this.handleQuestionChange = this.handleQuestionChange.bind(this)
+    this.handleAnswerChange = this.handleAnswerChange.bind(this)
+  }
+
+  // On form submit
+  handleNewQuestion() {
+    this.props.addNewQuestion(this.state)
+  }
+
+  // On question input change
+  handleQuestionChange(event) {
+    this.setState({ question: event.target.value });
+  }
+
+  // On answer input change
+  handleAnswerChange(event) {
+    this.setState({ answer: event.target.value });
+  }
+
   render() {
     return (
       <div className="new-question">
         <h1>New Question</h1>
 
-        <form>
+        <div>
           <div className="form-group">
             <label> Question </label>
             <input type="text"
                    className="form-control"
                    placeholder="Enter question"
+                   onChange={this.handleQuestionChange}
+                   value={this.state.question}
                    />
           </div>
           <div className="form-group">
             <label> Answer </label>
             <textarea className="form-control"
                       placeholder="Enter answer"
+                      onChange={this.handleAnswerChange}
+                      value={this.state.answer}
                       rows="3"></textarea>
           </div>
           <button type="submit"
-                  className="btn btn-primary">
+                  className="btn btn-primary"
+                  onClick={this.handleNewQuestion}>
             Submit
           </button>
-        </form>
+        </div>
 
       </div>
     );
   }
 }
 
-export default NewQuestion;
+// Map reducer methods
+const mapDispatchToProps = dispatch => ({
+  addNewQuestion: (payload) => dispatch(addNewQuestion(payload)),
+})
+
+export default connect(undefined, mapDispatchToProps)(NewQuestion)
