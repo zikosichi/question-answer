8a548d31a78ca6a4c9ecabe776b102b7f897441e
Zviad Sichinava <zviad.sichinava@devfactory.com>
2018-04-22 15:05:09 +0200

    faet: created questions component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import Questions from '../Questions/Questions';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello
+      <div className="container pt-5">
+        <div className="row">
+          <div className="col col-3">
+            Here You can find 5 questions
+          </div>
+          <div className="col col-auto">
+            <Questions></Questions>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Questions/Questions.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Questions.css';
+
+class Questions extends Component {
+  render() {
+    return (
+      <div className="questions">
+        Question Component
+      </div>
+    );
+  }
+}
+
+export default Questions;
